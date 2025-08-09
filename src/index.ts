977253385f6bf53b9130baf0224a04e074f30aa0
AlexRFarnes <alexrfarnes@gmail.com>
2025-08-08 22:43:41 -0700

    routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import path from "path";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
